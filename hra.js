72e5cf48c8058a10e53a1fce108a4c3e5ff7d7ba
Ilona Hůlková <ilca.hulkova@gmail.com>
2023-10-12 17:28:13 +0200

    celé pole X a O

--- hra.js
+++ hra.js
@@ -1,9 +1,10 @@
-
+import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4'
 
 const polickoElement = document.querySelector('.hraci_pole_policko');
 const ikonaHracElement = document.querySelector('.hra_image');
 
 let hracNaTahu = 'kolecko';
+
 
 const hracTah = (event) => {
   if (hracNaTahu === 'kolecko') {
@@ -19,14 +20,24 @@ const hracTah = (event) => {
   }
 };
 
-document.querySelector('#pole00').addEventListener('click', hracTah);
-document.querySelector('#pole01').addEventListener('click', hracTah);
-document.querySelector('#pole02').addEventListener('click', hracTah);
-document.querySelector('#pole03').addEventListener('click', hracTah);
-document.querySelector('#pole04').addEventListener('click', hracTah);
-document.querySelector('#pole05').addEventListener('click', hracTah);
-document.querySelector('#pole06').addEventListener('click', hracTah);
-document.querySelector('#pole07').addEventListener('click', hracTah);
-document.querySelector('#pole08').addEventListener('click', hracTah);
-document.querySelector('#pole09').addEventListener('click', hracTah);
+
+
+// 42-46
+const tahElement = document.querySelectorAll('.hraci_pole_policko')
+tahElement.forEach((btn) => {
+  btn.addEventListener('click', hracTah)
+})
+
+
+
+// document.querySelector('#pole00').addEventListener('click', hracTah);
+// document.querySelector('#pole01').addEventListener('click', hracTah);
+// document.querySelector('#pole02').addEventListener('click', hracTah);
+// document.querySelector('#pole03').addEventListener('click', hracTah);
+// document.querySelector('#pole04').addEventListener('click', hracTah);
+// document.querySelector('#pole05').addEventListener('click', hracTah);
+// document.querySelector('#pole06').addEventListener('click', hracTah);
+// document.querySelector('#pole07').addEventListener('click', hracTah);
+// document.querySelector('#pole08').addEventListener('click', hracTah);
+// document.querySelector('#pole09').addEventListener('click', hracTah);
 
