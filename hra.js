8d8d64012a6c93fde78ec49075119c42b85447ea
Ilona Hůlková <ilca.hulkova@gmail.com>
2023-10-21 12:31:35 +0200

    oprava

--- hra.js
+++ hra.js
@@ -1,10 +1,15 @@
 import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4'
 
-const polickoElement = document.querySelector('.hraci_pole_policko');
 const ikonaHracElement = document.querySelector('.hra_image');
 
 let hracNaTahu = 'kolecko';
 
+const restartButtonElement = document.querySelector('.znovu-tlacitko2')
+restartButtonElement.addEventListener('click', (event) => {
+  if (window.confirm('Opravdu chceš restartovat hru?') === false) {
+  event.preventDefault()
+  }
+})
 
 const hracTah = (event) => {
   if (hracNaTahu === 'kolecko') {
@@ -17,52 +22,51 @@ const hracTah = (event) => {
     event.target.disabled = true;
     ikonaHracElement.src = 'images/circle.svg';
     hracNaTahu = 'kolecko';
-  }
-
-// 16-25
-  let herniPole = []
-  const kosticky = document.querySelectorAll('.herni_pole_policko')
-  kosticky.forEach(kostka => {
-    let hodnota = kostka.textContent
-    if (hodnota === '') {
-      hodnota = '_'
-    }
-    herniPole.push(hodnota)
-  })
-
-// 29-40
-  const vysledek = findWinner(herniPole)
-  if (vysledek === 'images/circle_black.svg' || vysledek === 'images/cross_black') {
-    conslole.log('vyhrál', vysledek)
-
-  } else if (vysledek === 'tie') {
-    console.log('remiza')
-
-  } else {
-    console.log('zatím nic')
-  }
-};
-
-
-
-// 42-46
-const tahElement = document.querySelectorAll('.hraci_pole_policko')
-tahElement.forEach((btn) => {
-  btn.addEventListener('click', hracTah)
+ }
+ evaluation()
+}
+const gameArrays = document.querySelectorAll('.hraci_pole_policko')
+gameArrays.forEach((array) => {
+  array.addEventListener('click', hracTah)
 })
 
 
 
-// document.querySelector('#pole00').addEventListener('click', hracTah);
-// document.querySelector('#pole01').addEventListener('click', hracTah);
-// document.querySelector('#pole02').addEventListener('click', hracTah);
-// document.querySelector('#pole03').addEventListener('click', hracTah);
-// document.querySelector('#pole04').addEventListener('click', hracTah);
-// document.querySelector('#pole05').addEventListener('click', hracTah);
-// document.querySelector('#pole06').addEventListener('click', hracTah);
-// document.querySelector('#pole07').addEventListener('click', hracTah);
-// document.querySelector('#pole08').addEventListener('click', hracTah);
-// document.querySelector('#pole09').addEventListener('click', hracTah);
+const evaluation = () => {
+  let gameField = []
+  gameArrays.forEach((array) => {
+    if (array.classList.contains('hraci_pole_policko--kolecko')) {
+      gameField.push('o')
+    } else if (array.classList.contains('hraci_pole_policko--krizek')) {
+      gameField.push('x')
+    } else {
+      gameField.push('_')
+    }
+  })
 
+
+
+
+console.log(gameField)
+const winner = findWinner(gameField)
+console.log('winner', winner)
+
+if (winner === 'o') {
+  setTimeout(() => {
+    alert('Vyhrálo kolečko!')
+    location.reload()
+  }, 500)
+} else if (winner === 'x') {
+  setTimeout(() => {
+    alert('Vyhrál křížek!')
+    location.reload()
+  }, 500)
+} else if (winner === 'tie') {
+  setTimeout(() => {
+    alert('Hra skončila remízou!')
+    location.reload()
+  }, 500)
+}
+}
 
 
